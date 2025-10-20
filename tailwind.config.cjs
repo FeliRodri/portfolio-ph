ffffe90632c2ed6478c211707391bc9876d27396
Felipe Rodriguez <f.rodriguez.asenjo@gmail.com>
2025-10-20 11:34:27 -0300

    mejoras en la visualizacion del laboratorio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,13 +9,49 @@ module.exports = {
                         maxWidth: 'none',
                         color: 'inherit',
                         a: {
-                            color: 'inherit',
+                            color: '#2563eb',
                             textDecoration: 'none',
                             fontWeight: '500',
+                            '&:hover': {
+                                color: '#1d4ed8',
+                            },
                         },
                         'h1, h2, h3, h4': {
-                            color: 'inherit',
+                            color: '#18181b',
                             fontWeight: '700',
+                            marginTop: '1.5em',
+                            marginBottom: '0.5em',
+                        },
+                        hr: {
+                            borderColor: '#e4e4e7',
+                            marginTop: '2em',
+                            marginBottom: '2em',
+                        },
+                        ul: {
+                            listStyleType: 'disc',
+                            paddingLeft: '1.5em',
+                        },
+                        'ul > li': {
+                            paddingLeft: '0.5em',
+                            marginTop: '0.5em',
+                            marginBottom: '0.5em',
+                        },
+                        img: {
+                            borderRadius: '0.75rem',
+                            marginTop: '2em',
+                            marginBottom: '2em',
+                        },
+                        code: {
+                            backgroundColor: '#f4f4f5',
+                            borderRadius: '0.25rem',
+                            padding: '0.2em 0.4em',
+                            fontSize: '0.875em',
+                        },
+                        'code::before': {
+                            content: 'none',
+                        },
+                        'code::after': {
+                            content: 'none',
                         },
                     },
                 },
